46a520dadc0f7c3cb076efa986e5879c0ead1afb
moriya <moriya@test>
2023-07-17 12:06:05 +0900

    ショッピングリスト用の画面に変更

--- resources/views/task/list.blade.php
+++ resources/views/task/list.blade.php
@@ -1,24 +1,22 @@
 @extends('layout')
 
 {{-- タイトル --}}
-@section('title')
-(詳細画面)
-@endsection
+@section('title')(一覧画面)@endsection
 
 {{-- メインコンテンツ --}}
 @section('contets')
-        <h1>タスクの登録</h1>
-            @if (session('front.task_register_success') == true)
-                タスクを登録しました！！<br>
+        <h1>「買うもの」の登録</h1>
+            @if (session('front.shopping_list_register_success') == true)
+                「買うもの」を登録しました！！<br>
             @endif
-            @if (session('front.task_delete_success') == true)
-                タスクを削除しました！！<br>
+            @if (session('front.shopping_list_delete_success') == true)
+                「買うもの」を削除しました！！<br>
             @endif
-            @if (session('front.task_completed_success') == true)
-                タスクを完了にしました！！<br>
+            @if (session('front.shopping_list_completed_success') == true)
+                「買うもの」を完了にしました！！<br>
             @endif
-            @if (session('front.task_completed_failure') == true)
-                タスクの完了に失敗しました....<br>
+            @if (session('front.shopping_list_completed_failure') == true)
+                「買うもの」の完了に失敗しました....<br>
             @endif
             @if ($errors->any())
                 <div>
@@ -27,45 +25,32 @@
                 @endforeach
                 </div>
             @endif
-            <form action="/task/register" method="post">
+            <form action="/shopping_list/register" method="post">
                 @csrf
-                タスク名:<input name="name" value="{{ old('name') }}"><br>
-                期限:<input name="period" type="date" value="{{ old('period') }}"><br>
-                タスク詳細:<textarea name="detail">{{ old('detail') }}</textarea><br>
-                重要度:<label><input type="radio" name="priority" value="1" @if (old('priority') == 1) checked @endif>低い</label> /
-                    <label><input type="radio" name="priority" value="2" @if (old('priority', 2) == 2) checked @endif>普通</label> /
-                    <label><input type="radio" name="priority" value="3" @if (old('priority') == 3) checked @endif>高い</label><br>
-                <button>タスクを登録する</button>
+                「買うもの」名:<input name="name" value="{{ old('name') }}"><br>
+                <button>「買うもの」を登録する</button>
             </form>
 
-        <h1>タスクの一覧</h1>
-
-        <a href="/task/csv/download">CSVダウンロード</a><br>
-
-　　　　<div style="float: left; margin-right: 20px;">
-        <a href="/completed_tasks/list">完了タスク一覧</a>
-    </div><!-- 追記 -->
-
+        <h1>「買うもの」一覧</h1>
+        <a href="/completed_shopping_list/list">購入済み「買うもの」一覧</a><br>
         <table border="1">
         <tr>
-            <th>タスク名
-            <th>期限
-            <th>重要度
-@foreach ($list as $task)
+            <th>登録日
+            <th>「買うもの」名
+@foreach ($list as $shopping_item)
         <tr>
-            <td>{{ $task->name }}
-            <td>{{ $task->period }}
-            <td>{{ $task->getPriorityString() }}
-            <td><a href="{{ route('detail', ['task_id' => $task->id]) }}">詳細閲覧</a>
-            <td><a href="{{ route('edit', ['task_id' => $task->id]) }}">編集</a>
-            <td><form action="{{ route('complete', ['task_id' => $task->id]) }}" method="post"> @csrf <button onclick='return confirm("このタスクを「完了」にします。よろしいですか？");' >完了</button></form>
+            <td>{{ $shopping_item->created_at->format('Y/m/d') }}
+            <td>{{ $shopping_item->name }}
+            <td><form action="{{ route('complete', ['shopping_list_id' => $shopping_item->id]) }}" method="post"> @csrf <button onclick='return confirm("この「買うもの」を「完了」にします。よろしいですか？");' >完了</button></form></a>
+            <td>　
+            <td><form action="{{ route('delete', ['shopping_list_id' => $shopping_item->id]) }}" method="post"> @csrf @method("DELETE") <button onclick='return confirm("この「買うもの」を「削除」します。よろしいですか？");' >削除</button></form></a>
 @endforeach
         </table>
         <!-- ページネーション -->
         {{-- {{ $list->links() }} --}}
         現在 {{ $list->currentPage() }} ページ目<br>
         @if ($list->onFirstPage() === false)
-            <a href="/task/list">最初のページ</a>
+            <a href="/shopping_list/list">最初のページ</a>
         @else
             最初のページ
         @endif
