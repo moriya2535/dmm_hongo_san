dc83a88ad1c8ecdae350f1c2cbd350fe635d5809
moriya <moriya@test>
2023-07-19 21:22:54 +0900

    訂正

--- database/migrations/2021_12_08_090005_create_completed_shopping_lists_table.php
+++ database/migrations/2021_12_08_090005_create_completed_shopping_lists_table.php
@@ -15,11 +15,11 @@ class CreateCompletedShoppingListsTable extends Migration
     {
         Schema::create('completed_shopping_lists', function (Blueprint $table) {
             $table->unsignedInteger('id');
-            $table->string('name', 128);
-            $table->unsignedBigInteger('user_id');
+            $table->string('name', 128)->comment('「買うもの」名');
+            $table->unsignedBigInteger('user_id')->comment('この「買うもの」の所有者');
             $table->foreign('user_id')->references('id')->on('users'); // 外部キー制約
             //$table->timestamps();
-            $table->dateTime('created_at')->useCurrent();
+            $table->dateTime('created_at')->useCurrent()->comment('購入日時');
             $table->dateTime('updated_at')->useCurrent()->useCurrentOnUpdate();
             //
             $table->primary('id');
