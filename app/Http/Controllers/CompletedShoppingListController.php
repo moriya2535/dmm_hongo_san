f306548ec4ae21b9387216f518d9adc4d02fb650
moriya <moriya@test>
2023-07-20 23:02:50 +0900

    仕様に合わせて修正

--- app/Http/Controllers/CompletedShoppingListController.php
+++ app/Http/Controllers/CompletedShoppingListController.php
@@ -4,14 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Models\CompletedShoppingList as CompletedShoppingListModel;
 use Illuminate\Pagination\Paginator;
 
 class CompletedShoppingListController extends Controller
 {
+	protected function getListBuilder()
+    {
+        return CompletedShoppingListModel::where('user_id', Auth::id())
+                     ->orderBy('name')
+                     ->orderBy('created_at');
+    }
     public function list()
     {
-        $completedTasks = CompletedShoppingListModel::paginate(10); // 1ページあたり10件のタスクを表示
-    return view('task.completed_list', compact('completedTasks'));
+        $completedTasks = $this->getListBuilder()
+        					      ->paginate(10);
+        return view('task.completed_list', compact('completedTasks'));
     }
 }
